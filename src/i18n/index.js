684bbcd0fbca3a4109ad8b06fc16e596492e99b7
CodeNur <dodgechalanger9@gmail.com>
2026-04-20 17:46:56 +0500

    Sayt toliq tarjima qilindi

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,3 +1,4 @@
+//code written by codenur
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
@@ -9,9 +10,9 @@ import zh from "./locales/zh.json";
 
 export const LANGUAGE_MAP = {
   "O'zbek": "uz",
-  "Русский": "ru",
-  "English": "en",
-  "中文": "zh",
+  Русский: "ru",
+  English: "en",
+  中文: "zh",
 };
 
 export const LANGUAGE_DISPLAY = {
@@ -36,20 +37,18 @@ function getInitialLanguage() {
   return "uz";
 }
 
-i18n
-  .use(initReactI18next)
-  .init({
-    resources: {
-      uz: { translation: uz },
-      ru: { translation: ru },
-      en: { translation: en },
-      zh: { translation: zh },
-    },
-    lng: getInitialLanguage(),
-    fallbackLng: "uz",
-    interpolation: {
-      escapeValue: false,
-    },
-  });
+i18n.use(initReactI18next).init({
+  resources: {
+    uz: { translation: uz },
+    ru: { translation: ru },
+    en: { translation: en },
+    zh: { translation: zh },
+  },
+  lng: getInitialLanguage(),
+  fallbackLng: "uz",
+  interpolation: {
+    escapeValue: false,
+  },
+});
 
 export default i18n;
